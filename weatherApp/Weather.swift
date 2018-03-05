9815d857e643cf6a504a2d44fe9eac84ac81a331
[AHASSOUNI] <[nadia.ahassouni@pictime.com]>
2018-03-05 17:57:43 +0100

    first commit

---
+++ weatherApp/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  weatherApp
+//
+//  Created by Ahassouni, Nadia on 05/03/2018.
+//  Copyright © 2018 Ahassouni, Nadia. All rights reserved.
+//
+
+import Foundation
---
+++ weatherApp/WeatherGetter.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherGetter.swift
+//  weatherApp
+//
+//  Created by Ahassouni, Nadia on 05/03/2018.
+//  Copyright © 2018 Ahassouni, Nadia. All rights reserved.
+//
+
+import Foundation
